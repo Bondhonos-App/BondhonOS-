9942bdc26f2657bef0025427b28b5f0081189914
Bondhon OS™ <tomarghraanbd@gmail.com>
2025-08-31 13:21:16 +0600

    Add service-worker.js

---
+++ public /service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("bondhonos-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/desktop.js",
+        "/firebase.js",
+        "/manifest.webmanifest"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
