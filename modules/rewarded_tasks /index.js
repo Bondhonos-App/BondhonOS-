f8fd743cba7111a2e85c5eb1a567951df2a847ab
Bondhon OS™ <tomarghraanbd@gmail.com>
2025-09-08 19:39:35 +0600

    Add index.js

---
+++ modules/rewarded_tasks /index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rewarded Tasks</title>
+    <link rel="stylesheet" href="../core/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Rewarded Tasks</h1>
+    <button id="completeTask">Complete Task</button>
+    <script type="module" src="../core/script.js"></script>
+    <script type="module" src="script.js"></script>
+</body>
+</html>
