1c4202c6b27a06e881b3aa659baddd2d1c9d2111
Bondhon OS™ <tomarghraanbd@gmail.com>
2025-09-08 19:51:00 +0600

    Add index.js

---
+++ modules /referral_system /index.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Referral System</title>
+    <link rel="stylesheet" href="../core/style.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="referral-container">
+        <h1>Referral System</h1>
+        <p>Invite your friends and earn rewards!</p>
+        <input type="text" id="referralLink" readonly>
+        <button id="copyLink">Copy Referral Link</button>
+        <button id="shareLink">Share Link</button>
+        <div id="statusMessage"></div>
+    </div>
+
+    <script type="module" src="../core/script.js"></script>
+    <script type="module" src="script.js"></script>
+</body>
+</html>দ
