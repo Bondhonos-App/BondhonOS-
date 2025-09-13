70843fd32303b30108916086fa63878919450c99
Bondhon OS™ <224091319+Bondhonos-App@users.noreply.github.com>
2025-09-13 19:24:50 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,5 @@
-// firebase-config.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-
-// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDA3ibp9sbmuQfiPWx--GrCdROmQB92YLc",
+  apiKey: "AIzaSyCP8RjRCubcRcnUrRT7dWFaNMeoqC2hxeo", // নতুন Key
   authDomain: "bhondhun-os-tm.firebaseapp.com",
   projectId: "bhondhun-os-tm",
   storageBucket: "bhondhun-os-tm.appspot.com",
@@ -13,11 +7,3 @@ const firebaseConfig = {
   appId: "1:130936996088:web:50584da37eb951b0a13b0d",
   measurementId: "G-XD2M104XGV"
 };
-
-// Initialize Firebase only once
-const app = initializeApp(firebaseConfig);
-
-// Export Auth, Firestore, and Google Auth Provider
-export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-export const db = getFirestore(app);
