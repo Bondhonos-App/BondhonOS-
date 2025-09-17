9bdd1dd2f4c48ae7f7dffa8d872f0b9b4f33aa7d
Bondhon OS™ <224091319+Bondhonos-App@users.noreply.github.com>
2025-09-17 06:13:58 +0600

    Add Login.js

---
+++ frontend/src/Pages/Login.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    alert(`Login request: ${email}`);
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
