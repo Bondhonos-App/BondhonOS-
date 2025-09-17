77a2e9fcf5a92b49e76edc63711450d9af05c0d5
Bondhon OS™ <224091319+Bondhonos-App@users.noreply.github.com>
2025-09-17 07:01:19 +0600

    Add Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ padding: "10px", background: "#eee", textAlign: "center" }}>
+      <p>© 2025 BondhonOS. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
